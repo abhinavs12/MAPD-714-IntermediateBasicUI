20359fd86eb7fd6d01696c180297a2b3250d852b
Student <student@setass-mac-mini-41.local>
2017-09-20 20:32:48 -0400

    Initial Commit

---
+++ MAPD-714-Intermediate-User-Interactions/ViewController.swift
@@ -0,0 +1,25 @@
+/*
+ * Name: Abhinav Sharma
+ * Date: September 20, 2017
+ * StudentID: 300975677
+ * Description: Intermediate User Interaction Demo
+ * Version: 0.1 - Built Basic UI
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
