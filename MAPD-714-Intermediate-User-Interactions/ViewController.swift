2b1f901f3e327f232f8db68eeef92733a9492d8b
Student <student@setass-mac-mini-41.local>
2017-09-20 20:40:30 -0400

    First Commt

--- MAPD-714-Intermediate-User-Interactions/ViewController.swift
+++ MAPD-714-Intermediate-User-Interactions/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //OUTLETS+++++++++++
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    
+    
+    
+    //INHERITED METHODS ++++++++++
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //ACTION METHODS
+    @IBAction func onSliderChange(_ sender: UISlider) {
+        
+        
+        
     }
-
+    
+    @IBAction func onSegmentedControlChange(_ sender: UISegmentedControl) {
+        
+        
+    }
+    
 
 }
 
